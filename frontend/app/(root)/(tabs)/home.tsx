0288e7e3e878ac2ac18181b9e18de68355897a3d
Md Saim Ahmmed Utsho <saim.cse.du.744@gmail.com>
2024-10-08 17:33:39 +0600

    router added to navigate between pages and bottom navigation bar implementation started

---
+++ frontend/app/(root)/(tabs)/home.tsx
@@ -0,0 +1,12 @@
+import { Link } from "expo-router";
+import { FlatList, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+export default function Page() {
+  return (
+    <SafeAreaView>
+      <Text>Hello world</Text>
+    </SafeAreaView>
+  );
+}
