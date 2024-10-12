59b4754ac4322073cba99d4235ed9049d7aab0d9
Md Saim Ahmmed Utsho <saim.cse.du.744@gmail.com>
2024-10-12 12:37:10 +0600

    camera fuctionality will be implemented soon

--- frontend/app/(root)/(tabs)/camera.tsx
+++ frontend/app/(root)/(tabs)/camera.tsx
@@ -1,10 +1,20 @@
-import { View, Text } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { View, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Chat = () => {
   return (
-    <SafeAreaView>
-      <Text>Camera</Text>
+    <SafeAreaView className="">
+      <TouchableOpacity
+        onPress={() => router.push("/(meal)/scan-food")}
+        className="pt-72 items-center"
+      >
+        <FontAwesome5 name="camera" size={32} color="green" />
+        <Text className="mt-2 text-sm font-semibold text-black">
+          Open up Camera
+        </Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
