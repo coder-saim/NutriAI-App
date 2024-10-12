b865aed2c641bd7971ba9430271976f64eae3062
Md Saim Ahmmed Utsho <saim.cse.du.744@gmail.com>
2024-10-13 00:08:19 +0600

    suggested meals screen updated with functionality

--- frontend/app/(root)/(services)/(meal)/suggested-meals.tsx
+++ frontend/app/(root)/(services)/(meal)/suggested-meals.tsx
@@ -56,19 +56,24 @@ const MyMealsPage = () => {
 
   return (
     <ScrollView className="flex-1 bg-white p-4">
-      <View className="flex-row justify-between mb-4">
+      <View className="flex-row ml-1 mb-4">
         <TouchableOpacity
           onPress={() => router.push("/(meal)/my-meals")}
-          className="bg-green-100 border border-green-300 px-4 py-2 rounded-md"
+          className="bg-green-100 border border-green-300 px-14 py-3 rounded-l-lg"
         >
           <Text className="text-black text-center font-bold">My Meals</Text>
         </TouchableOpacity>
-        <TouchableOpacity className=" bg-[#159339] px-4 py-2 rounded-md">
+        <TouchableOpacity
+          onPress={() => router.push("/(meal)/suggested-meals")}
+          className="bg-[#159339] px-7 py-3 rounded-r-lg"
+        >
           <Text className="text-white text-center font-bold">
             Suggested Meals
           </Text>
         </TouchableOpacity>
       </View>
+
+      
 
       <View className="flex-row justify-around mb-4">
         {["Breakfast", "Lunch", "Dinner", "Snacks"].map((meal, index) => (
