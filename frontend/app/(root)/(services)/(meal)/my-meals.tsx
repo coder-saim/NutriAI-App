f9499707c5277a17eb2665862252356d8d92a77d
Md Saim Ahmmed Utsho <saim.cse.du.744@gmail.com>
2024-10-13 00:08:06 +0600

    my meals screen updated with functionality

--- frontend/app/(root)/(services)/(meal)/my-meals.tsx
+++ frontend/app/(root)/(services)/(meal)/my-meals.tsx
@@ -57,13 +57,13 @@ const MyMealsPage = () => {
 
   return (
     <ScrollView className="flex-1 bg-white p-4">
-      <View className="flex-row justify-between mb-4">
-        <TouchableOpacity className="bg-[#159339] px-4 py-2 rounded-md">
+      <View className="flex-row ml-1 mb-4">
+        <TouchableOpacity className="bg-[#159339] px-14 py-3 rounded-l-lg">
           <Text className="text-white text-center font-bold">My Meals</Text>
         </TouchableOpacity>
         <TouchableOpacity
           onPress={() => router.push("/(meal)/suggested-meals")}
-          className="bg-green-100 border border-green-300 px-4 py-2 rounded-md"
+          className="bg-green-100 border border-green-300 px-7 py-3 rounded-r-lg"
         >
           <Text className="text-black text-center font-bold">
             Suggested Meals
