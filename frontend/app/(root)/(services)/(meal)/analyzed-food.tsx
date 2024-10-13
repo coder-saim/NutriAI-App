e9f28437d3cac4aa613fcad5dec51280f9403302
Md Saim Ahmmed Utsho <saim.cse.du.744@gmail.com>
2024-10-13 10:21:54 +0600

    analyzed food screen updated

--- frontend/app/(root)/(services)/(meal)/analyzed-food.tsx
+++ frontend/app/(root)/(services)/(meal)/analyzed-food.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
 import { images } from "@/constants";
 import { router } from "expo-router";
 
