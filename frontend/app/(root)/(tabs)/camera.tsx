12f7570bc577bdd459d1b72370838a2715f648f3
Md Saim Ahmmed Utsho <saim.cse.du.744@gmail.com>
2024-10-13 10:21:06 +0600

    camera screen updated

--- frontend/app/(root)/(tabs)/camera.tsx
+++ frontend/app/(root)/(tabs)/camera.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { View, Text, Button, Alert, TouchableOpacity } from "react-native";
+import { View, Image, Text, Button, Alert, TouchableOpacity } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import * as MediaLibrary from "expo-media-library";
 import { router } from "expo-router";
+import { images } from "@/constants";
 
 const CameraScreen = () => {
   const openImagePicker = async () => {
@@ -63,14 +64,18 @@ const CameraScreen = () => {
   };
 
   return (
-    <View className="flex-1 justify-center items-center">
-      <Button title="Pick an image" onPress={showPickerOptions} />
+    <View className="flex-1 pb-48 bg-white justify-center items-center">
+      <View className="items-center mt-12 mb-8">
+        <Image source={images.scan} className="w-64 h-64" />
+        <Text className="text-lg font-bold text-center">Capture an image of your food to analyze the nutritional contents.</Text>
+      </View>
+      {/* <Button title="Pick an image" onPress={showPickerOptions} /> */}
       {["Open Camera", "Choose image from Gallery"].map((buttonType, index) => (
         <TouchableOpacity
           key={index}
           className="bg-[#159339] w-80 py-3 my-2 rounded-full"
           onPress={() => {
-            index == 0 ? openCamera() : openImagePicker()
+            index == 0 ? openCamera() : openImagePicker();
           }}
         >
           <Text className="text-white text-center text-lg font-bold">
