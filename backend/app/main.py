c7280cdedee9f6c45873587d5bdf136ef2bb979c
Md Saim Ahmmed Utsho <saim.cse.du.744@gmail.com>
2024-10-15 23:43:47 +0600

    registration, login and food testing api end point created

---
+++ backend/app/main.py
@@ -0,0 +1,30 @@
+from fastapi import Depends, FastAPI, HTTPException, Response, status
+from . import models
+from .database import engine
+from .routers import user, auth, food
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+
+app.include_router(user.router)
+app.include_router(auth.router)
+app.include_router(food.router)
+
+
