e01db22672bb9d11e7d2306d12dad8bb4def591b
Sadmin <sadmid26tahmid@gmail.com>
2024-10-15 14:05:43 +0600

    complete settings page layout

--- frontend/app/(root)/(tabs)/(profile)/settings.tsx
+++ frontend/app/(root)/(tabs)/(profile)/settings.tsx
@@ -1,11 +1,59 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
+import { icons, images } from '@/constants'
+import SettingsButton from '@/components/SettingsButton'
 
 const Settings = () => {
+
+  const profileList = [
+    {
+      id: 1,
+      Name: "Language",
+      image: icons.profileGreen,
+      toggle: true
+    },
+    {
+      id: 2,
+      Name: "Dark Mode",
+      image: icons.starGreen,
+      toggle: false
+    },
+    {
+      id: 3,
+      Name: "Location",
+      image: icons.settings,
+      toggle: false
+    },
+    {
+      id: 4,
+      Name: "Notification",
+      image: icons.logout,
+      toggle: false
+    },
+    {
+      id: 5,
+      Name: "Help Center",
+      image: icons.bookmark,
+      toggle: true
+    },
+    {
+      id: 6,
+      Name: "FAQ",
+      image: icons.bookmark,
+      toggle: true
+    },
+  ]
+
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex h-full items-center bg-white">
+        <View className="items-start w-full px-6 mt-5">
+          {profileList.map((item) => (
+            <SettingsButton key={item.id} Name={item.Name} icon={item.image} toggle={item.toggle}/>  
+          ))}
+        </View>
+      </View>
+    </ScrollView>
   )
 }
 
--- frontend/components/SettingsButton.tsx
+++ frontend/components/SettingsButton.tsx
@@ -3,19 +3,15 @@ import React from 'react';
 import arrowRight from '@/assets/icons/arrow-right.png';
 import { router } from 'expo-router';
 
-export default function ProfileButton({ Name, icon, page }: { Name: string, icon: any, page: string }) {
-
-  const onOptionPress = async () => {
-    router.push(`/(root)/(tabs)/(profile)/${page}`);
-  };
+export default function SettingsButton({ Name, icon, toggle }: { Name: string, icon: any, toggle: boolean }) {
 
   return (
-    <TouchableOpacity onPress={onOptionPress} className='mt-6 flex-row items-center space-x-4'>
+    <TouchableOpacity className='mt-6 flex-row items-center space-x-4'>
       <View className='w-12 h-12 bg-[#bcf9bc] rounded-lg justify-center items-center'>
         <Image source={icon} className='w-6 h-6' />
       </View>
       <Text className='text-left text-xl font-medium flex-1'>{Name}</Text>
-      <Image source={arrowRight} className='w-8 h-8 ml-auto'/>
+      {toggle && <Image source={arrowRight} className='w-8 h-8 ml-auto'/>}
     </TouchableOpacity>
   );
 }
