b18ff9b391a95fc632ab8e296367f134e7e0bb8e
Md Saim Ahmmed Utsho <saim.cse.du.744@gmail.com>
2024-10-16 11:01:58 +0600

    update profile button added

--- frontend/app/(root)/(tabs)/(profile)/edit-profile.tsx
+++ frontend/app/(root)/(tabs)/(profile)/edit-profile.tsx
@@ -1,8 +1,9 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { ScrollView } from "react-native";
 import { icons, images } from "@/constants";
 import InputField from "@/components/InputField";
+import { router } from "expo-router";
 
 const editProfile = () => {
   return (
@@ -31,6 +32,14 @@ const editProfile = () => {
             textContentType="telephoneNumber"
             labelOn={true}
           />
+          <TouchableOpacity
+            className="bg-[#159339] w-80 py-3 mt-2 rounded-full mb-6"
+            onPress={() => router.push("/main-profile")}
+          >
+            <Text className="text-white text-center text-lg font-bold">
+              Update Profile
+            </Text>
+          </TouchableOpacity>
         </View>
       </View>
     </ScrollView>
