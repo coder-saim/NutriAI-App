5679fe814931095d27fc2175dc2f719044815f93
Md Saim Ahmmed Utsho <saim.cse.du.744@gmail.com>
2024-10-18 23:45:31 +0600

    generating otp and sending email functionality implemented

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,4 +1,10 @@
 from passlib.context import CryptContext
+import random
+import smtplib
+from email.message import EmailMessage
+from .config import settings
+
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
@@ -8,3 +14,29 @@ def hash_password(password: str):
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+
+
+
+def get_otp_code(to_mail: str):
+    otp = ""
+    for _ in range(6):
+        otp += str(random.randint(0,9))
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+
+    from_mail = settings.secret_email
+    server.login(from_mail, settings.app_token)
+    
+    msg = EmailMessage()
+    msg['Subject' ] = "OTP Verification"
+    msg['From' ]= from_mail
+    msg['To'] = to_mail
+    msg.set_content("Your OTP is: " + otp)
+
+    server.send_message(msg)
+    
+    return otp
+
+    
