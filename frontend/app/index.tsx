f62f1f38bf19fe21ca15f61fce94f15eb6f41280
Md Saim Ahmmed Utsho <saim.cse.du.744@gmail.com>
2024-10-20 00:43:25 +0600

    shared preferences implemented

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,7 +1,35 @@
+import { useState, useEffect } from "react";
 import { Redirect } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { View, ActivityIndicator } from "react-native";
 
 const Homepage = () => {
-  return <Redirect href="/(auth)/welcome" />;
+  const [isLoading, setIsLoading] = useState(true);
+  const [authToken, setAuthToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const checkAuthToken = async () => {
+      const token = await AsyncStorage.getItem("authToken");
+      setAuthToken(token);
+      setIsLoading(false);
+    };
+
+    checkAuthToken();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View className="mt-96">
+        <ActivityIndicator size="large" color="#159339" />
+      </View>
+    );
+  }
+
+  return authToken ? (
+    <Redirect href="/(root)/(tabs)/home" />
+  ) : (
+    <Redirect href="/(auth)/welcome" />
+  );
 };
 
 export default Homepage;
