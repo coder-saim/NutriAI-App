a3015c13bd96fddb0899d3beb69f498e45b72986
Md Saim Ahmmed Utsho <saim.cse.du.744@gmail.com>
2024-10-21 00:55:41 +0600

    sign up api added to front end

--- frontend/app/(auth)/sign-up.tsx
+++ frontend/app/(auth)/sign-up.tsx
@@ -1,20 +1,28 @@
 import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
 import { icons, images } from "@/constants";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 import { Link, router } from "expo-router";
 import { useState } from "react";
-import { View, Text, Image, ScrollView, Alert } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  Alert,
+  ToastAndroid,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const SingUp = () => {
-  const [signIn, setActive] = useState();
+  const [strength, setStrength] = useState(0);
   const isLoaded = false;
   const [showSuccessModal, setShowSuccessModal] = useState(false);
 
   const [form, setForm] = useState({
     name: "",
     email: "",
-    phone: "",
     password: "",
   });
   const [verification, setVerification] = useState({
@@ -23,11 +31,83 @@ const SingUp = () => {
     code: "",
   });
 
-  const onSignUpPress = async () => {
-    router.push("/(auth)/email-verify");
+  const validateEmail = (email: string) => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
   };
 
-  const onPressVerify = async () => {};
+  const validatePasswordStrength = (password: string) => {
+    const minLength = /.{8,}/; // At least 8 characters
+    const hasUpperCase = /[A-Z]/; // At least one uppercase letter
+    const hasLowerCase = /[a-z]/; // At least one lowercase letter
+    const hasNumber = /[0-9]/; // At least one digit
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/; // At least one special character
+
+    if (!minLength.test(password)) {
+      ToastAndroid.show(
+        "Password at least 8 characters long!",
+        ToastAndroid.SHORT
+      );
+      return true;
+    } else setStrength(strength + 1);
+    if (!hasUpperCase.test(password)) {
+      ToastAndroid.show(
+        "Password at least one uppercase letter!",
+        ToastAndroid.SHORT
+      );
+      return true;
+    } else setStrength(strength + 1);
+    if (!hasLowerCase.test(password)) {
+      ToastAndroid.show(
+        "Password at least one lowercase letter!",
+        ToastAndroid.SHORT
+      );
+      return true;
+    } else setStrength(strength + 1);
+    if (!hasNumber.test(password)) {
+      ToastAndroid.show("Password at least one digit!", ToastAndroid.SHORT);
+      return true;
+    } else setStrength(strength + 1);
+    if (!hasSpecialChar.test(password)) {
+      ToastAndroid.show(
+        "Password at least one special character!",
+        ToastAndroid.SHORT
+      );
+      return true;
+    } else setStrength(strength + 1);
+  };
+
+  const onSignUpPress = async () => {
+    if (!form.name || !form.email || !form.password) {
+      ToastAndroid.show(
+        "Name, Email or Password can not be empty!",
+        ToastAndroid.SHORT
+      );
+      return;
+    }
+
+    if (!validateEmail(form.email)) {
+      ToastAndroid.show("Email should be valid!", ToastAndroid.SHORT);
+      return;
+    }
+
+    if (validatePasswordStrength(form.password)) {
+      return;
+    }
+
+    await axios
+      .post("http://192.168.1.7:8000/users/register", form)
+      .then(async (response) => {
+        console.log("Sign Up successful:", response.status);
+        if (response.status == 201) {
+          await AsyncStorage.setItem("authToken", JSON.stringify(true));
+          router.push("/(auth)/email-verify");
+        }
+      })
+      .catch((error) => {
+        console.error("Login failed:", error);
+      });
+  };
 
   return (
     <ScrollView className="flex bg-white">
@@ -50,14 +130,7 @@ const SingUp = () => {
             value={form.name}
             onChangeText={(value) => setForm({ ...form, name: value })}
           />
-          <InputField
-            label="Phone"
-            placeholder="Enter phone number"
-            icon={icons.phone}
-            textContentType="telephoneNumber"
-            value={form.phone}
-            onChangeText={(value) => setForm({ ...form, phone: value })}
-          />
+
           <InputField
             label="Email"
             placeholder="Enter email"
@@ -75,6 +148,7 @@ const SingUp = () => {
             value={form.password}
             onChangeText={(value) => setForm({ ...form, password: value })}
           />
+
           <CustomButton
             title="Sign Up"
             onPress={onSignUpPress}
