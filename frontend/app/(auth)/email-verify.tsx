fc2b31040283592b9c7d82c715ff8fbe5131bc71
Md Saim Ahmmed Utsho <saim.cse.du.744@gmail.com>
2024-10-29 22:44:44 +0600

    email verification api integrated

--- frontend/app/(auth)/email-verify.tsx
+++ frontend/app/(auth)/email-verify.tsx
@@ -1,23 +1,42 @@
 import OtpVerification from "@/components/OtpVerification";
 import React from "react";
-import { router } from "expo-router";
-
-
+import { router, useLocalSearchParams } from "expo-router";
+import axios from "axios";
+import { baseURL } from "@/constants/api";
+import { ToastAndroid } from "react-native";
 
 export default function EmailVerification() {
+  const { email } = useLocalSearchParams<{ email: string }>();
 
-  const handleOtpVerify = (otp: number) => {
-    console.log("OTP Verified:", otp); 
-    router.replace("/(profile-building)/more-info");
-    // Handle OTP verification logic here
+  const handleOtpVerify = async (otp: number) => {
+    // console.log("OTP Verified:", otp, email);
+    await axios
+      .post(`${baseURL}/users/otp_verification`, {
+        email: email,
+        input_otp: otp,
+      })
+      .then(async (response) => {
+        console.log("OTP verification successfull: ", response.status);
+        if (response.status == 200) {
+          ToastAndroid.show(
+            "OTP verification successfull!",
+            ToastAndroid.SHORT
+          );
+          router.replace("/(profile-building)/more-info");
+        }
+      })
+      .catch((error) => {
+        console.log("OTP verification failed:", error);
+        ToastAndroid.show("Invalid OTP!", ToastAndroid.SHORT);
+      });
   };
 
   return (
     <OtpVerification
-      email="john@gmail.com"
+      email={email}
       onVerify={handleOtpVerify}
-      otpLength={6} 
-      timerDuration={30} 
+      otpLength={6}
+      timerDuration={30}
     />
   );
 }
