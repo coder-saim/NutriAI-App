bbb448b0d609ea1a829215f6246fded81a025b5d
Md Saim Ahmmed Utsho <saim.cse.du.744@gmail.com>
2024-10-29 22:45:10 +0600

    reset password verification api integrated

--- frontend/app/(auth)/reset-password-verify.tsx
+++ frontend/app/(auth)/reset-password-verify.tsx
@@ -1,16 +1,42 @@
 import OtpVerification from "@/components/OtpVerification";
-import { router } from "expo-router";
+import { baseURL } from "@/constants/api";
+import axios from "axios";
+import { router, useLocalSearchParams } from "expo-router";
 import React from "react";
+import { ToastAndroid } from "react-native";
 
 export default function ResetPasswordVerification() {
-  const handleOtpVerify = (otp: number) => {
+  const { email } = useLocalSearchParams<{ email: string }>();
+
+  const handleOtpVerify = async (otp: number) => {
     console.log("OTP Verified:", otp);
-    router.replace("/(auth)/reset-password-now")
+    await axios
+      .post(`${baseURL}/users/otp_verification`, {
+        email: email,
+        input_otp: otp,
+      })
+      .then(async (response) => {
+        console.log("OTP verification successfull: ", response.status);
+        if (response.status == 200) {
+          ToastAndroid.show(
+            "OTP verification successfull!",
+            ToastAndroid.SHORT
+          );
+          router.push({
+            pathname: "/(auth)/reset-password-now",
+            params: { email: email },
+          });
+        }
+      })
+      .catch((error) => {
+        console.log("OTP verification failed:", error);
+        ToastAndroid.show("Invalid OTP!", ToastAndroid.SHORT);
+      });
   };
 
   return (
     <OtpVerification
-      email="john@gmail.com"
+      email={email}
       onVerify={handleOtpVerify}
       otpLength={6}
       timerDuration={30}
